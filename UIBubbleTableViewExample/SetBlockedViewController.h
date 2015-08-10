7ab9d0ab794d7cd8e13fb47216898088cd8d99b7
Imane Rai <imane0310@gmail.com>
2015-08-10 13:37:30 +0930

    Release under MIT public license

--- UIBubbleTableViewExample/SetBlockedViewController.h
+++ UIBubbleTableViewExample/SetBlockedViewController.h
@@ -3,7 +3,7 @@
 //  Serval Chat
 //
 //  Created by Imane Rai on 11/05/2015.
-//  Copyright (c) 2015 Imane Rai. All rights reserved.
+//  Copyright (c) 2015 Imane Rai. Licensed to the public under the terms set out in the LICENSE.txt file at the top of this distribution.
 //
 
 #import <UIKit/UIKit.h>
